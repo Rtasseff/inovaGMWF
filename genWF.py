8193df280c8b5666bf7df1655a41c8297c85b8ba
rtasseff <rtasseff@gmail.com>
2014-11-11 13:29:05 +0100

    run mk gnmc batch features, merge and pairwise on titan is good

--- genWF.py
+++ genWF.py
@@ -21,7 +21,7 @@ are mapped to FAM when appropriate.
 """
 
 import numpy as np
-
+import subprocess
 
 
 floatFmtStr = '%05.4E'
@@ -29,7 +29,6 @@ floatFmtStr = '%05.4E'
 
 def _checkPatientID(patientID,studyID='101',allowedSuffix=['FAM']):
 	"""Check that ID is consistent with expectations"""
-	print patientID
 	tmp = patientID.split('-')
 	if tmp[0] != studyID:
 		raise ValueError ("ERR:0001 unexpected study id "+str(tmp[0]))
@@ -50,29 +49,110 @@ def getPatientOrder(fmPath,studyID='101',allowedSuffix=['FAM']):
 		_checkPatientID(patient,studyID=studyID,allowedSuffix=allowedSuffix)	
 	return patients
 
-def _parseCGSampID(sampID,studyID='101'):
+def getRepNBList(nbListPath,):
+	"""Create a list (np str array) that contains
+	all usable newborns for family centric analysis.
+	Used in maping individuals to -FAM
+	for family centric analsyis.
+	Tab sep input list should have all and only
+	newborns used to represent a family
+	unit. An error will be returned if
+	a family id xxx in 101-xxx-NB is 
+	included twice.
+
+	The list will be used for simplicity;
+	effectivly -F -M and -NB are 
+	just replaced with -FAM. However,
+	for multi births only the one
+	in the list will be used in the 
+	-FAM family centric analysis.
+	"""
+	nbList = np.loadtxt(nbListPath,dtype=str)
+	n = len(nbList)
+	used = np.zeros(n,dtype=int)
+	# check
+	for i in range(n):
+		nb = nbList[i]
+		if not nb.split('-')[2]=='NB':
+			raise ValueError('ERR0012:non newborn id '+nb+' found in nb list at '+nbListPath)
+		famID = int(nb.split('-')[1])
+		if np.any(famID==used):
+			raise ValueError('ERR0013:duplicate family ids '+str(famID)+' found in nb list at '+nbListPath)
+		used[i] = famID
+	return(nbList)
+
+def _itmiID2isbID(itmiID):
+	"""ITMI puts member ids as prefixes, but ISB puts
+	them as sufixes.  This will echange that order.
+	Note that nan will be returned if member id is 
+	not yet accounted for.  For example sample
+	PO-101-657-22 has PO member id that is 
+	not accounted for in any logic so far or
+	analysis done. Note that the 3 sets 
+	of number complicates maters with this ID.
+	"""
+	isbID = 'na'
+	tmp = itmiID.split('-')
+	# hard coded allowed prefixes 
+	if tmp[0] in ['F','M','NB']:
+		if len(tmp)==3:
+			isbID = tmp[1]+'-'+tmp[2]+'-'+tmp[0]
+		if len(tmp)==4:
+			isbID = tmp[2]+'-'+tmp[3]+'-'+tmp[0]+'-'+tmp[1]
+	return(isbID)
+
+def _mapInd2FAM(indID,nbList):
+	"""effectivly -F and -M are 
+	just replaced with -FAM. However,
+	for multi births only the one
+	in the list will be used in the 
+	-FAM family centric analysis.
+	The nbList will be used to deterimine
+	if the passed individual is nb and if so
+	is the representitve nb from the list.
+	Returns the new member id and new -FAM id.
+	Non representitve nb's or unrecognized 
+	member ids will return na.
+	indID	str, isb fomrated id for individual smaple
+	nbList	np str array of usable representitive new borns
+	"""
+	membID = 'na'
+	famID = 'na'
+	tmp = indID.split('-')
+	if len(tmp)==3 and tmp[2] in ['F','M']:
+		membID = tmp[2]
+		famID = tmp[0]+'-'+tmp[1]+'-FAM'
+	# only checking for multi births which have 4 '-'
+	elif np.any(indID==nbList):
+		famID = tmp[0]+'-'+tmp[1]+'-FAM'
+		membID = 'NB'
+	return(famID,membID)
+		
+
+
+
+
+
+
+	
+
+
+def _parseCGSampID(itmiID,nbList,studyID='101'):
+	"""Parse the CG file sample id (equivlant to itmi id)
+	into a usable isb id.  Currently coded for family centric analysis 
+	and returns the family id and seperatly the member (if id is usable, na otherwise).
+	itmiID	str, itmi id from CG file
+	nbList	np str array of usable representitive new borns
+	studyID	str the expected study id, for checking
+	"""
 	# assumes <member>(optional,-<member part 2>)-<study id>-<trio id>
 	# returns the format <study ID>-<family ID>-<representitive member [NB,NB-A]>
 	# memb can be used to check if this is a valid member ie NB,NB-1,M,F 
 
 	if studyID != '101': raise ValueError('ERR:0008, Study not indicated to be 101, logic for 101 maps all features to family centric feature matrix')
-	tmp = sampID.strip().split('-')
-	membID = -1
-	
-	# the sample id is diffrent form ours + its more variable (diffren pre and suffixes)
-	# map to family ID
-	if len(tmp) == 4: 
-		memb = tmp[0]+'-'+tmp[1]
-		sampIDNew = tmp[2]+'-'+tmp[3]+'-FAM'
-		# its possible this parsing does not work, but that is only 
-		# in cases where the family member is not part of the 
-		# 101 family centric study, and the sample will return 
-		# an inappropriate member ID, to be used by caller to ignore sample ID
-	else: 
-		memb = tmp[0]
-		sampIDNew = tmp[1]+'-'+tmp[2]+'-FAM' # if this is wrong it will be caught via memb
+	isbID = _itmiID2isbID(itmiID)
 
-	return(sampIDNew, memb)
+	return(_mapInd2FAM(isbID,nbList))
 
 def _parseVersion(versionStr):
 	# parse the version string form the CG manifest file
@@ -90,20 +170,26 @@ def _paresShipDate(shipDateStr):
 	return int(tmp)
 
 
-def parseMkGenomeBatchFM(manPath,fmPath,patients,genFeatName="GNMC:BATCH"):
+def parseMkGenomeBatchFM(manPath,fmPath,samples,nbList,studyID='101',genFeatName="GNMC:BATCH"):
 	"""Parse the source files for the 
 	genomic batch information, currently
 	the manifest file only.
 	Then make and save the batch feature matrix.
+	Currently creating a family centric feautre matrix,
+	relevent to study 101, so features not mapable to a 
+	family unit (ie -FAM) will be ignored.
 	manPath 	str, path to input manifest file
 	fmPath 	str, path to output genomic batch feature matrix
-	patients	list of patients ids as strings 
+	samples	list of sample ids as strings 
 			related to primary feature matrix
+	nbList	np str array of usable representitive new borns
+	studyID	str the expected study id, for checking
+
 	genFeatName 	str, the general name of the these batch features
 	"""
 	# get data from the manifest
 	# mainifest is small, use whats easiest (in mem, np arrays):
-	patients = np.array(patients,dtype=str)
+	samples = np.array(samples,dtype=str)
 	data = np.loadtxt(manPath,dtype=str,delimiter='\t')
 	labels = data[0]
 	data = data[1:]
@@ -131,18 +217,18 @@ def parseMkGenomeBatchFM(manPath,fmPath,patients,genFeatName="GNMC:BATCH"):
 	# hard code the possible number of mebers, corrisponding the the feautres above
 	nMembers = 3
 
-	nPatients = len(patients)
+	nSamples = len(samples)
 
 	# create blank FM
-	fm = np.array((np.zeros((nFeat*nMembers,nPatients)) + np.nan),dtype='|S15') # making it an str allows us to control format as we go and use non numerical data types if needed later.
+	fm = np.array((np.zeros((nFeat*nMembers,nSamples)) + np.nan),dtype='|S15') # making it an str allows us to control format as we go and use non numerical data types if needed later.
 
 	# loop through all data
 	for i in range(n):
 		# get sample ID
-		sampID, memb = _parseCGSampID(data[i,sampInd])
+		sampID, memb = _parseCGSampID(data[i,sampInd],nbList,studyID)
 		# hard code the memb index to conform to order above in FAM centric FM
 		membInd = -1
-		if memb=='NB' or memb=='NB-A': membInd = 0
+		if memb=='NB': membInd = 0
 		elif memb=='M': membInd = 1
 		elif memb=='F': membInd = 2
 		# ignore members that do not map to predefined family unit
@@ -150,19 +236,19 @@ def parseMkGenomeBatchFM(manPath,fmPath,patients,genFeatName="GNMC:BATCH"):
 			# version
 			iFeat = 0
 			row = iFeat * nMembers + membInd
-			fm[row,sampID==patients] = '%i'%(_parseVersion(data[i,versionInd]))
+			fm[row,sampID==samples] = '%i'%(_parseVersion(data[i,versionInd]))
 			
 
 			# ship date
 			iFeat = 1
 			row = iFeat * nMembers + membInd
-			fm[row,sampID==patients] = '%i'%(_paresShipDate(data[i,dateInd]))
+			fm[row,sampID==samples] = '%i'%(_paresShipDate(data[i,dateInd]))
 
 	# save the final matrix
-	saveFM(fm,fmPath,featHeader,patients)
+	saveFM(fm,fmPath,featHeader,samples)
 
 	
-def saveFM(data,fmPath,featHeader,patients):
+def saveFM(data,fmPath,featHeader,samples):
 	"""Given np arrays, save the feature matrix in 
 	standard format.
 	"""
@@ -170,10 +256,10 @@ def saveFM(data,fmPath,featHeader,patients):
 	n,m = data.shape
 	if n!=len(featHeader):
 		raise ValueError('ERR:0009, number of features (rows) and headers not equal')
-	if m!=len(patients):
+	if m!=len(samples):
 		raise ValueError('ERR:00010, number of patients and col not equal')
 	fout.write('.\t')
-	fout.write('\t'.join(patients)+'\n')
+	fout.write('\t'.join(samples)+'\n')
 	for i in range(n):
 		fout.write(featHeader[i]+'\t'+'\t'.join(np.array(data[i],dtype=str))+'\n')
 
@@ -241,18 +327,32 @@ def catFM(fMNames,sampleIDs,foutPath,studyID='101',allowedSuffix=['FAM']):
 
 			
 
+def runPairwise(fMPath,outDir,outName,pwWhich='/titan/cancerregulome8/TCGA/scripts/pairwise-2.0.0-current'):
+	"""Run the pairwise code found at pwWhich on feature matrix at
+	fMName and save the output to outName.
+	"""
+	call = pwWhich+' '+fMPath+' '+outDir+'/'+outName
+	# redirecting output to an info file
+	with open(outDir+"/pwTmp.out",'w') as stdout:
+		subprocess.check_call(call,shell=True,stdout=stdout)
+
+
 
 
 
 
 def main():
-	fmInPath = '/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/2014_10_17_genomic_clinical_hilevel_RT.fm'
-	fmPath = '/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/test.fm'
-	manPath = '/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/P286_Data_Delivery_TOTAL_021214.txt'
-	trioList = getPatientOrder(fmInPath)
-	parseMkGenomeBatchFM(manPath,fmPath,trioList)
-	catFM(['/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/test.fm','/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/DF5_Methylation_Batch_Vars.txt'],trioList,'/Users/RTasseff/Projects/INOVA/DF5/basePipeLine/test_2.fm',studyID='101')
-
+	clinFM = '/titan/ITMI1/projects/gamcop/data/featureMatrices/data_CLIN_20141111.fm'
+	gnmcBatchFM = '/titan/ITMI1/projects/gamcop/data/featureMatrices/metadata_GNMC_20141111.fm'
+	wrkDir = '/users/rtasseff/inova/inovaMWF'
+	nbListPath = wrkDir+'/repNBList.tsv'
+	outFM = wrkDir+'/test.fm'
+	manPath = wrkDir+'/P286_Data_Delivery_TOTAL_021214.txt'
+	nbList = getRepNBList(nbListPath)
+	samples = getPatientOrder(clinFM)
+	parseMkGenomeBatchFM(manPath,gnmcBatchFM,samples,nbList)
+	catFM([gnmcBatchFM,clinFM],samples,outFM,studyID='101')
+	runPairwise(outFM,wrkDir,'pairwiseTestOut.dat')
 
 if __name__ == '__main__':
 	main()
