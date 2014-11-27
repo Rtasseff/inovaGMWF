471dcfb8aaff24169467f9251d993a19b17ba420
rtasseff <rtasseff@gmail.com>
2014-11-27 11:00:06 +0100

    added method to count QC metrics over complete vcf file; untested.

--- gnmcUtil.py
+++ gnmcUtil.py
@@ -10,7 +10,8 @@ RAT 20141121
 
 import numpy as np
 import genUtil 
-
+import sys
+import gzip
 
 import warnings
 
@@ -113,6 +114,98 @@ def parseMkGenomeBatchFM(manPath,fmPath,samples,genFeatName="GNMC:BATCH"):
 
 	# save the final matrix
 	genUtil.saveFM(fm,fmPath,featHeader,samples)
+
+
+def getQCFM_VCF(vcfPath,fmOutPath,genFeatName="GNMC:QC",sampStartInd = 9):
+	"""Get the predetermined QC features from vcf at vcfPath 
+	(use gzip if path ends in .gz) and create and individual
+	sample feature matrix at fmOutPath. genFeatName is a str
+	of the general name of the these QC features
+	<Data Source>:<Feature Class>
+	current features:
+	MIE_count - total number of annotated MIEs over genome
+	VQLow_count - total number of annotated VQLow calls over genome
+	Missing_count - total number of missing calls over genome
+	homoz_count - total number of homozygous calls 
+	heteroz_count - total number of heterozygous calls
+	Note are parsing assumptions:
+	- last comment line is sample line
+	- sample line first entry #CHROM
+	- sample line has sampStartInd entries before the IDs start
+	- col of variant line correspond with sample line
+	- once variant lines start they continue to eof
+	- vcf is tab sep
+
+	"""
+
+	# to ensure consistency
+	mieInd = 0
+	vqlowInd = 1
+	missInd = 2
+	homoInd = 3
+	hetroInd = 4
+	nFeat = 5
+
+	featName = np.array(np.zeros(nFeat),dtype='|S60')
+	featName[miwInd] = 'N:'+genFeatName+':MIE_count'
+	featName[vqlowInd] = 'N:'+genFeatName+':VQLow_count'
+	featName[missInd] = 'N:'+genFeatName+':miss_count'
+	featName[homoInd] = 'N:'+genFeatName+':homoz_count'
+	featName[hetroInd] = 'N:'+genFeatName+':heteroz_count'
+
+	
+	
+	if vcfPath[-2:]=='gz':
+		vcf = gzip.open(vcfPath,"r")
+	else:
+		vcf = open(vcfPath,"r")
+	# keep track of position, top header
+	readLine = False
+	for line in vcf: ### in vcf -->
+		col = line.strip().split('\t')
+		if not readLine:
+			# check if this is needed comment 
+			if col[0]=='#CHROM':
+				sampleList = np.array(col[sampStartInd:],dtype=str)
+				nSamp = len(sampleList)
+				data = np.zeros((nFeat,nSamp))
+				# verify these are known sample ids formats
+				known=True
+				for samp in sampleList:
+					idType = genUtil.idType(samp)
+					if idType=='na':
+						raise ValueError("ERR:0008, found an unknown sample ID format in "+vcfPath+", vcf may be incorrect, format may need to be added to known formats, or sampStartInd may need to be changed.")
+
+		else:  #### in readable section, variant line -->
+			calls = col[sampStartInd:]
+			# check len
+			if len(calls)!=nSamp:
+				raise ValueError("ERR:0009, number of calls and samples does not match in "+vcfPath
+			for i in range(nSamp):
+				err = False
+				if calls[i].find(".") <> -1: 
+					data[missInd,i] += 1
+					err = True
+				if calls[i].find("VQLOW") <> -1: 
+					data[vqlowInd,i] += 1
+					err = True
+				if calls[i].find("MIE") <> -1: 
+					data[mieInd,i] += 1
+					err = True
+				if not err:
+					tmp = calls[i].split('/')
+					if len(tmp)==2:
+						if tmp[0]==tmp[1]:data[homoInd,i] += 1
+						else: data[hetroInd,i] += 1
+
+				
+
+
+		#### <-- in readable section, variant line
+	### <-- in vcf
+
+	# done with vcf save feature matrix
+	saveFM(data,fmOutPath,featName,sampleList)
 
 
 def main():
