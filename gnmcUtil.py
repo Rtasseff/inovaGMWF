9c8b40c8558af59dde6966c5391a6c110b7a9d71
rtasseff <rtasseff@gmail.com>
2014-12-23 13:48:06 +0100

    created methods and classes to read binary region files and supporting informaiton, ie handeling output of split transcirpts

--- gnmcUtil.py
+++ gnmcUtil.py
@@ -12,7 +12,7 @@ import numpy as np
 import genUtil 
 import sys
 import gzip
-
+import struct
 import warnings
 
 
@@ -213,6 +213,142 @@ def getQCFM_VCF(vcfPath,fmOutPath,genFeatName="GNMC:QC",sampStartInd = 9):
 	# done with vcf save feature matrix
 	genUtil.saveFM(data,fmOutPath,featName,sampleList)
 
+def summQCRegionFM(qcRegionDir,qcRegionName,numQCRegions,outQCFMPath):
+	"""Used to sum up all individual qc regions into a single FM,
+	very adhoc logic here:
+	loops though all regions to sum up a single FM
+	for QC variables, assumes that all files are 
+	in the same dir with <qcRegionName>_i.fm 
+	where i = 1,2...,numQCRegions
+	"""
+
+	for i in range(1,numQCRegions+1):
+		fm = np.loadtxt(qcRegionDir+'/'+qcRegionName+'_'+str(i)+'.fm',dtype=str,delimiter='\t')
+		samples_tmp = list(fm[0,1:])
+		features_tmp = list(fm[1:,0])
+		if i==1:
+			samples = samples_tmp
+			features = features_tmp
+			data = np.array(fm[1:,1:],dtype=float)
+		else:
+			if (features_tmp != features) or (samples_tmp != samples):
+				raise ValueError('FM labels not the same in '+qcRegionName+'_'+str(i)+'.fm when adding up QC FMs')
+			data += np.array(fm[1:,1:],dtype=float)
+		
+	genUtil.saveFM(data,outQCFMPath,features,samples)
+
+def readBiMat(path,nRow):
+	"""Reads a bianary format and spits out a matrix.
+	assumes 8 bit unsigned char, will put it 
+	an np array by dividing into nRows
+	type = unsigned 8 bit integer
+	Sets all '0' values to nan, which is the 
+	convention on split transcripts.
+	"""
+	# open and read the bianary file
+	tmp = open(path,'rb').next()
+	# one byte for each char 
+	nData = len(tmp)
+	# get col
+	nCol = nData/nRow 
+
+	# need to setup format for conversion
+	fmt = str(nData)+'B'
+	# save data as np array, uint8
+	data = np.array(struct.unpack(fmt,tmp),dtype='uint8')
+	# put into matrix 
+	X = np.reshape(data,(nRow,nCol))
+	X[X==0] = np.nan
+	
+	return(X)
+
+class RegionManifestReader:
+	"""Reads the file at the given path as the mainfest of the 
+	regions that were identifed by split-transcripts.  
+	Can iterate through using next(), returns a region.
+	Assumes the paths in the mainfest for the data are 
+	relative to the same dir as the manifest,
+	otherwise you can specify the relative dir path
+	on initilization.
+	Must close the reder like a file object when done.
+	"""
+	# setting for parsing the manifest file from split-transcripts
+	delim = '\t'
+	commentStr = '#'
+	dataDirPrePath =''
+	# index for the col of each value
+	IND_NAME = 0
+	IND_CHROM = 1
+	IND_START = 2
+	IND_STOP = 3
+	IND_NUM = 4
+	IND_DATA_PATH = 5
+	def __init__(self,path,dataDirPrePath=''):
+		self.fin=open(path)
+		# the paths in the manifest are relative paths, lets deal with that here
+		# check to see if a dataDirPrePath is specified or needs to be parsed
+		if dataDirPrePath=='':
+			tmp = path.split('/')
+			if len(tmp)>1:self.dataDirPrePath = '/'.join(tmp[:-1])+'/'
+		else:
+			if dataDirPrePath[-1]!='/':dataDirPrePath=dataDirPrePath+'/'
+			self.dataDirPrePath = dataDirPrePath
+	def next(self):
+		# search for the next readable region 
+		regionLine = False
+		while not regionLine:
+			line = self.fin.next()
+			if line[0] != self.commentStr:
+				values = line.strip().split(self.delim)
+				if len(values)==6:regionLine=True
+		# create and pass back the region
+		name = values[self.IND_NAME]
+		chrom = values[self.IND_CHROM]
+		startPos= values[self.IND_START]
+		stopPos = values[self.IND_STOP]
+		nVar = values[self.IND_NUM]
+		dataPath = self.dataDirPrePath+values[self.IND_DATA_PATH]
+		region = Region(name,chrom,startPos,stopPos,nVar,dataPath)
+		return region
+
+
+	def close(self):
+		self.fin.close()
+
+	def __iter__(self):
+		return self
+
+
+
+class Region:
+	"""Represents a region with all relevent information
+	including a funciton to load the corrisponding region data.
+	"""
+	name = ''
+	chrom = ''
+	startPos = ''
+	stopPos = ''
+	nVar = ''
+	dataPath = ''
+	def __init__(self,name,chrom,startPos,stopPos,nVar,dataPath):
+		self.name = name
+		self.chrom = chrom
+		self.startPos = startPos
+		self.stopPos = stopPos
+		self.nVar = nVar
+		self.dataPath = dataPath
+
+	def getData():
+		"""Get the corrisponding data matrix for this transcript 
+		from the binary matrix.
+		"""
+		readBiMat(self.dataPath,self.nVar)
+
+
+
+				
+
+	
 
 def main():
 #	# test functions
@@ -228,11 +364,32 @@ def main():
 #
 #	genUtil.indFM2FamFM(fmPath,famFMPath,nbList,famSample)
 
-	# get the QC FM from comand line
-	vcfPath = sys.argv[1]
-	fmOutPath = sys.argv[2]
+	# --QC feautres 
 
-	getQCFM_VCF(vcfPath,fmOutPath)
+#	# -get the QC FM of regions from comand line ->
+#	# **done seperatly
+#	# in this case each region is done individually,
+#	# need to call the command in a loop from terminal
+#	vcfPath = sys.argv[1]
+#	fmOutPath = sys.argv[2]
+#	getQCFM_VCF(vcfPath,fmOutPath)
+#	# <-
+
+	# -addup QC regions ->
+	# done seperatly 
+	# loops though all regions to sum up a single FM
+	# for QC variables, assumes that all files are 
+	# in the same dir with <qcRegionName>_i.fm 
+	# where i = 1,2...,numQCRegions
+	qcRegionDir = sys.argv[1]
+	qcRegionName = sys.argv[2]
+	numQCRegions = sys.argv[3]
+	outQCFMPath = sys.argv[4]
+	summQCRegionFM(qcRegionDir,qcRegionName,numQCRegions,outQCFMPath)
+	# <-
+
+
+
 
 if __name__ == '__main__':
 	main()
