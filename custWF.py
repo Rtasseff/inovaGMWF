e3367ab871b5f5c0bb82ee7f630481dd9dc3493c
rtasseff <rtasseff@gmail.com>
2015-01-30 13:00:40 +0100

    add option to not use FDR liter, added main code to do single small FM for use in external loop

--- custWF.py
+++ custWF.py
@@ -10,14 +10,13 @@ import random
 
 PWPATH_TITAN = '/titan/cancerregulome8/TCGA/scripts/pairwise-2.0.0-current'
 PWPATH_SGI = '/isb/rkramer/bin/pairwise-2.0.1'
-MAXQ = .1
 
-def run2FMPWwList(FM1Path,FM2Path,pwOutPath,outDir,pwWhich=PWPATH_SGI,samples='',maxQ=MAXQ,testFListPath=''):
+def run2FMPWwList(FM1Path,FM2Path,pwOutPath,outDir,pwWhich=PWPATH_SGI,samples='',maxQ=.1,testFListPath=''):
 	"""Run pairwise between two existing FMs.
 	FM1 and FM2 considered test and target features, respectively.
 	Procedure creates tmp files in outDir then removes them. 
 	PW output saved to pwOutPath. 
-	Output is filtered by FDR q values with a max set in maxQ.
+	If maxQ>0 output is filtered by FDR q values with a max set in maxQ.
 	Uses PW code at pwWhich.
 	If samples == '', use FM1 header row to define sample list,
 	else if samples is a path, load a tsv sample list from that path,
@@ -57,28 +56,40 @@ def run2FMPWwList(FM1Path,FM2Path,pwOutPath,outDir,pwWhich=PWPATH_SGI,samples=''
 	FMTmpPath = outDir+'/tmpFM_'+str(random.randrange(16**5))+'.tsv'
 	genWF.catFM([FM1Path,FM2Path],samples,FMTmpPath,checkSampIDs=False)
 
+	if maxQ < 0: maxQ = ''
+
 	genWF.runPairwise(FMTmpPath,outDir,pwOutPath,pwWhich=pwWhich,pairFile=pairListPath,fdrFilter=maxQ)
 
 	# get list of test variables
 	if testFListPath!='':
-		pwOut = open(pwOutPath)
-		testFList = [line.strip().split('\t')[0] for line in pwOut]
-		pwOut.close()
-		# remove repeats:
-		testFList = list(set(testFList))
-		testFListFile = open(testFListPath,'w')
-		for value in testFList:
-			# ignore comment lines
-			if value[0]!='#':testFListFile.write(value+'\n')
-		testFListFile.close()
-	
+		
 	os.remove(pairListPath)
 	os.remove(FMTmpPath)
 
 	
 
+def getSimpleList(pwOutPath,testFListPath):
+	"""Make a simple non-redundant list
+	of the test features (0th column of 
+	the pairwise output at pwOutPath)
+	and save it to testFListPath.
+	"""
+	pwOut = open(pwOutPath)
+	testFList = [line.strip().split('\t')[0] for line in pwOut]
+	pwOut.close()
+	# remove repeats:
+	testFList = list(set(testFList))
+	testFListFile = open(testFListPath,'w')
+	for value in testFList:
+		# ignore comment lines
+		if value[0]!='#':testFListFile.write(value+'\n')
+	testFListFile.close()
+
+
 
 def main():
+
+	# run a single massive pairwise for getting the batch list
 	outDir = '/isb/rtasseff/results/var_batch_20150128'
 	testFMPath = '/isb/rtasseff/data/featureMatrices/DF5_MergedVCF_ForPairwise_ExcludingPO.fm'
 	targFMPath = '/isb/rtasseff/data/featureMatrices/BATCH_GNMC_IND_20150109.fm'
@@ -86,7 +97,24 @@ def main():
 	listOutPath = outDir+'/var_batchFilter_featureNameList_20150128.dat'
 	samplePath = '/isb/rtasseff/data/support/sampleIDList_ind_DF5_itmiFormat.dat'
 
-	run2FMPWwList(testFMPath,targFMPath,fullOutPath,outDir,maxQ=MAXQ,testFListPath=listOutPath,samples=samplePath)		
+	run2FMPWwList(testFMPath,targFMPath,fullOutPath,outDir,maxQ=.1,testFListPath=listOutPath,samples=samplePath)		
+
+	# run multiple small pairwise to get batch results, 
+	# assumes you are using the PBS vector job option 
+	# after PBS run you must cat, FDR and get list separately 
+	tag = sys.argv[1]
+	outDir = '/isb/rtasseff/results/var_batch_20150202'
+	testFMPath = '/bigdata0/users/vdhankani/GNMC_FM/'+tag+'.Filtered.fm.gz'
+	targFMPath = '/isb/rtasseff/data/featureMatrices/BATCH_GNMC_IND_20150109.fm'
+	fullOutPath = outDir+'/fullPWOut_'+tag+'.dat'
+	samplePath = '/isb/rtasseff/data/support/sampleIDList_ind_DF5_itmiFormat.dat'
+
+	
+	run2FMPWwList(testFMPath,targFMPath,fullOutPath,outDir,maxQ=-1,samples=samplePath)		
+
+
+
+
 
 if __name__ == '__main__':
 	main()
